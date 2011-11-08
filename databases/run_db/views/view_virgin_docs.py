edf3f65c40f2018a38a1d1333156050860f8c018
Michael Marino <mmarino@gmail.com>
2011-11-08 16:57:32 +0100

    Updating virgin docs and it's associated update script.

--- databases/run_db/views/view_virgin_docs.py
+++ databases/run_db/views/view_virgin_docs.py
@@ -4,9 +4,12 @@ def get_view_class():
     return ViewDefinition("analysis", "all_virgin_docs", \
     '''function(doc) {
         if (doc.raw_data_file_tier_0 &&
-            (!doc.raw_data_file_tier_0.last_mod_time ||
-            !doc.root_data_file_tier_1.last_mod_time ||
-            !doc.output_data_file_tier_2.last_mod_time)) {
+            doc.raw_data_file_tier_0.length == 0 &&
+            doc.root_data_file_tier_1 &&
+            doc.root_data_file_tier_1.length == 0 &&
+            doc.output_data_file_tier_2 &&
+            doc.output_data_file_tier_2.length == 0
+            ) {
             emit(doc._id, null);
         }
      }
