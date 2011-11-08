c414a464baaca1fe40c9e711604d52fb9d3abd61
Michael Marino <mmarino@gmail.com>
2011-11-08 16:57:51 +0100

    Updating view of all the runs, this is not necessary to have an LN2 check.

--- databases/run_db/views/view_all_runs.py
+++ databases/run_db/views/view_all_runs.py
@@ -4,7 +4,6 @@ def get_view_class():
     return ViewDefinition("analysis", "all_runs", \
     '''function(doc) {
        var my_string = doc._id
-       if (my_string.search("LN2") != -1) return;
        if (isNaN(parseInt(my_string))) return;
        emit(doc._id, null); 
     }
