c26683da68512c88f366445843065d105322b603
Michael Marino <mmarino@gmail.com>
2011-11-09 18:20:34 +0100

    Updating update_calculations on db so that it is a little smarter and distributes jobs a little more cleanly.  The idea is to distribute across different cpus as much as possible.

--- update/update_calculations_on_database.py
+++ update/update_calculations_on_database.py
@@ -4,6 +4,7 @@ from ..utilities.utilities import detectCPUs
 import os
 import sys
 import numpy
+import time
 
 
 def run_update_calc( server_build, alist, update_rundoc):
@@ -59,42 +60,54 @@ def update_calculations_on_database():
     num_cpus = detectCPUs() 
     print "Performing the following updates: " 
     must_cycle = False
-    for amod in module_list: 
-        # Get the view to use
-        view = amod.get_view()
-        # Get the list of docs needing update
-        list_of_docs = view(server.get_database())
-        print "  %s" % amod.__name__
+    pid_lock_docs = {}
+    def get_run_docs_to_update(module_list, ser):
+        list_to_return = {} 
+        for amod in module_list: 
+            # Get the view to use
+            view = amod.get_view()
+            # Get the list of docs needing update
+            list_of_docs = view(ser.get_database())
+            for doc in list_of_docs:  
+                if doc.id not in list_to_return: 
+                    list_to_return[doc.id] = [amod]
+                elif amod not in list_to_return[doc.id]:
+                    list_to_return[doc.id].append(amod)
+        return list_to_return
+   
+    process_list = get_run_docs_to_update(module_list, server)
+    while len(process_list) != 0:
+        for doc, mods in process_list.items(): 
+            while len(pid_lock_docs) >= num_cpus:  
+                # we have to wait for some to finish 
+                pid,_ = os.waitpid(-1, 0)
+                if pid in pid_lock_docs:
+                    del pid_lock_docs[pid]
+            if doc not in pid_lock_docs.values(): 
+                pid = os.fork()
+                if pid: # parent
+                    pid_lock_docs[pid] = doc
+                else: # child process
+                    del server
+                    # The following function calls amod.update_rundoc 
+                    # on all the records in alist
+                    run_update_calc(RunServer, [doc], mods[0].update_rundoc)
+                    sys.exit(0)
+        # get a new process list
+        # Check if any children have died and collect them
+        pid,_ = os.waitpid(-1, os.WNOHANG)
+        if pid in pid_lock_docs:
+            del pid_lock_docs[pid]
+        process_list = get_run_docs_to_update(module_list, server)
+        time.sleep(5)
 
-        # Now split up the list for number of cpus
-        total_list = numpy.array([id.id for id in list_of_docs])
-        if len(total_list) > 0: must_cycle = True
-
-        all_lists = [total_list[i::num_cpus] for i in range(num_cpus)]
-
-        # ship out to different threads
-        # We use a fork because the amount of work done
-        # by the child should be fairly significant, making
-        # the amount of time spent to fork negligible.  
-        thread_list = []
-        for alist in all_lists: 
-            if len(alist) == 0: continue # Don't send out for 0 length lists
-            pid = os.fork()
-            if pid: # parent
-                thread_list.append(pid)
-            else: # child process
-                del server
-                # The following function calls amod.update_rundoc 
-                # on all the records in alist
-                run_update_calc(RunServer, alist, amod.update_rundoc)
-                sys.exit(0)
-                # stop here for the child process
-
-        # Wait for worker children to complete
-        for thread in thread_list:
-            os.waitpid(-1, 0)
+    while len(pid_lock_docs) != 0:
+        pid,_ = os.waitpid(-1, 0)
+        if pid in pid_lock_docs:
+            del pid_lock_docs[pid]
 
     server.reset_lock()
+    server.remove_stale_processing_files()
     if must_cycle:
         print "Some documents were updated, cycling again to resolve all updates"
         update_calculations_on_database()
