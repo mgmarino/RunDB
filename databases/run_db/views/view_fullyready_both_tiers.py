731600dab668bb5658aa9f02fb2ae64cd5b8d740
Michael Marino <mmarino@gmail.com>
2011-12-01 14:28:24 +0100

    Fixing fully ready views.

--- databases/run_db/views/view_fullyready_both_tiers.py
+++ databases/run_db/views/view_fullyready_both_tiers.py
@@ -2,19 +2,32 @@ from couchdb.design import ViewDefinition
 
 def get_view_class():
     return ViewDefinition("analysis", "all_fully_ready_both_tiers", \
-    '''function(doc) {
+    '''
+function(doc) {
         var temp = doc.output_data_file_tier_2;
         var temp1 = doc.root_data_file_tier_1;
         if (!temp || !temp1 ||
             temp.length == 0 || 
             temp1.length != temp.length) return;
+
         for (var i=0;i<temp.length;i++) {
             if (!temp[i].pfn || !temp1[i].pfn) {
                 return;
             }
         }
+        var adict = {};
+        var match = /.*-([0-9]*)\.root/;
         for (var i=0;i<temp.length;i++) {
-            emit(doc._id, [temp[i].pfn, temp1[i].pfn]);
+            var tempmatch = match.exec(temp[i].pfn);
+            var tempmatch1 = match.exec(temp1[i].pfn);
+            if (!tempmatch || !tempmatch1) continue;
+            if (!(tempmatch[1] in adict)) adict[tempmatch[1]] = ['', ''];
+            if (!(tempmatch1[1] in adict)) adict[tempmatch1[1]] = ['', ''];
+            adict[tempmatch[1]][0] = temp[i].pfn;
+            adict[tempmatch1[1]][1] = temp1[i].pfn;
+        }
+        for (var key in adict) {
+            emit(doc._id, adict[key]);
         }
      }
     ''')
