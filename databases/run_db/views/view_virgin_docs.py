04ae60200561b2b240f350f2fdfd6c70cfd7b2f8
Michael Marino <mmarino@gmail.com>
2011-12-01 14:26:56 +0100

    Updates for the virgin run view.

--- databases/run_db/views/view_virgin_docs.py
+++ databases/run_db/views/view_virgin_docs.py
@@ -3,12 +3,12 @@ from couchdb.design import ViewDefinition
 def get_view_class():
     return ViewDefinition("analysis", "all_virgin_docs", \
     '''function(doc) {
-        if (doc.raw_data_file_tier_0 &&
-            doc.raw_data_file_tier_0.length == 0 &&
-            doc.root_data_file_tier_1 &&
-            doc.root_data_file_tier_1.length == 0 &&
-            doc.output_data_file_tier_2 &&
-            doc.output_data_file_tier_2.length == 0
+        if ((doc.raw_data_file_tier_0 &&
+            doc.raw_data_file_tier_0.length == 0 )||
+            (doc.root_data_file_tier_1 &&
+            doc.root_data_file_tier_1.length == 0) ||
+            (doc.output_data_file_tier_2 &&
+            doc.output_data_file_tier_2.length == 0)
             ) {
             emit(doc._id, null);
         }
